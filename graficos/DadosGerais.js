a864827c9b171153030c57a9b4e3348092db8c87
GabzinhoHD <134400952+GabzinhoHD@users.noreply.github.com>
2024-09-25 12:04:31 -0300

    Add files via upload

--- graficos/DadosGerais.js
+++ graficos/DadosGerais.js
@@ -1,1 +1,18 @@
-const url ="https://raw.githubusercontent.com/GabzinhoHD/Banco-de-dados-EX/refs/heads/main/entrevista.json"
+const url = 'https://raw.githubusercontent.com/GabzinhoHD/Banco-de-dados-EX/refs/heads/main/entrevista.json'
+
+async function visualizarInfos() {
+    const res = await fetch(url)
+    const dados = await res.json()
+    const SalgadosMais = dados[0].salgado
+    const NumeroVotos = dados[0].votos
+    const comentarios = dados[0].comentario
+    console.log(dados[0].comentario)
+
+    const paragrafo = document.createElement("p")
+    paragrafo.classList.add("caixa-grafico__texto")
+    paragrafo.innerHTML = `Essa pesquisa, buscou fazer um levantamento dos salgados mais consumidos nas cantinas das escola pelo mundo. Com o auxilio da inteligencia artificial, foi possivel concluir que o salgado ${SalgadosMais} ficou em primeiro lugar, com aproximadamente ${NumeroVotos} votos, o principal comentario arespeito do salgado foi: ${comentarios}`
+    const caixa = document.getElementById("caixa-grafico")
+    caixa.appendChild(paragrafo)
+}
+
+visualizarInfos()
