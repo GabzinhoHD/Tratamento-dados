ec0451f669ec5b5779dfb1c55c1a6e2a8d982169
GabzinhoHD <134400952+GabzinhoHD@users.noreply.github.com>
2024-10-02 10:37:33 -0300

    Add files via upload

--- graficos/GraficoBarra.js
+++ graficos/GraficoBarra.js
@@ -1,8 +1,21 @@
 async function criargraficoBarra() {
-    const url = "https://raw.githubusercontent.com/GabzinhoHD/Banco-de-dados-EX/refs/heads/main/entrevista.json"
+    const url = "https://raw.githubusercontent.com/GabzinhoHD/Banco-de-dados-EX/refs/heads/main/dadosbarra.json"
     const res = await fetch(url)
     const dados = await res.json()
-    console.log(dados)
+    const salgados = Object.keys(dados)
+    const votos = Object.values(dados)
+
+    const data = [
+        {
+            x: salgados,
+            y: votos,
+            type: "bar"
+        }
+    ]
+    const grafico = document.createElement("div")
+    grafico.className = "grafico"
+    document.getElementById("caixa-grafico").appendChild(grafico)
+    Plotly.newPlot(grafico, data)
  }
 
  criargraficoBarra()
