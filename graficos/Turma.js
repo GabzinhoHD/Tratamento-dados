c69901b568d985fd47d6ce292e5e3f246948145c
GabzinhoHD <134400952+GabzinhoHD@users.noreply.github.com>
2024-10-09 12:14:27 -0300

    Add files via upload

--- graficos/Turma.js
+++ graficos/Turma.js
@@ -1,15 +1,14 @@
 import { pegarCss } from "./comum.js"
 
-async function criargraficoTancredo() {
+async function criargraficoTurma() {
     const url = "https://raw.githubusercontent.com/GabzinhoHD/Banco-de-dados-EX/refs/heads/main/Tancredo.json"
     const res = await fetch(url)
     const dados = await res.json()
-    const BebidasVotadas = dados.slice(1).map( bebidas => bebidas[2] )
-    const Contagem = BebidasVotadas.reduce((acc, BebidasVotadas ) => {
-        acc[BebidasVotadas] = (acc[BebidasVotadas] || 0) + 1 
+    const turma = dados.slice(1).map( Turmas => Turmas[1] )
+    const Contagem = turma.reduce((acc, turma ) => {
+        acc[turma] = (acc[turma] || 0) + 1 
         return acc
     },{})
-    console.log(Contagem)
     const valores = Object.values(Contagem)
     const etiqueta = Object.keys(Contagem)
     const data = [
@@ -32,7 +31,7 @@ async function criargraficoTancredo() {
 
     const pesquisaTitulo = document.createElement("h3")
     pesquisaTitulo.classList.add("caixa-grafico__texto")
-    pesquisaTitulo.innerHTML = `As bebidas mais votadas no Colegio <span>Tancredo</span>`
+    pesquisaTitulo.innerHTML = `As turmas do colegio <span>Tancredo</span> que votaram`
     const grafico = document.createElement("div")
     grafico.className = "grafico"
     document.getElementById("caixa-grafico").appendChild(pesquisaTitulo)
@@ -42,12 +41,6 @@ async function criargraficoTancredo() {
         displayModeBar: false
     }
     Plotly.newPlot(grafico, data, layout, config)
-
-    const caixa = document.getElementById("caixa-grafico")
-    const paragrafo = document.createElement("p")
-    paragrafo.classList.add("caixa-grafico__texto")
-    paragrafo.innerHTML = `Observa-se que a bebida mais votada no <span>colégio</span> é diferente da mais votada no mundo. A <span>água</span> no mundo foi a mais votada, em comparação no colégio foi o <span>refrigerante</span>, já a <span>água ficou em quarto lugar</span> aatrás dos sucos de laranja e uva.`
-    caixa.appendChild(paragrafo)
 }
 
-criargraficoTancredo()
+criargraficoTurma()
